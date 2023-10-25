7fa3375374c919ebe2fc089a7dd541e180d844ab
longwu <longwu@longwu.xyz>
2023-10-25 23:44:39 +0800

    add rotate

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -12,8 +12,8 @@ export default function HomePage() {
       </Head>
       <div className="h-[100vh] p-10">
         <Tooltip title="新建作品" color={'#2db7f5'}>
-        <div className='w-[160px] h-[220px] bg-[#fefefed2] rounded-2xl shadow-2xl cursor-pointer flex items-center justify-center'>
-          <img src='/images/add.png' alt='add' width={64} />
+        <div className='w-[160px] h-[220px] bg-[#fefefed2] rounded-2xl shadow-2xl cursor-pointer flex items-center justify-center group'>
+          <img src='/images/add.png' alt='add' width={64} className='transform group-hover:rotate-90 transition duration-500' />
         </div>
         </Tooltip>
         
