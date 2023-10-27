748416d823c18efd433498975c70850fddbb036a
longwu <longwu@longwu.xyz>
2023-10-27 22:31:21 +0800

    update UI

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -24,32 +24,33 @@ export default function HomePage() {
       <Head>
         <title>龙屋</title>
       </Head>
-      <div className="h-[100vh] p-10 flex flex-row flex-wrap">
-        {
-          books.sort((a,b)=>(a.createTime - b.createTime)).map((book, index)=>{
-            return (
-              <div key={book.title + '_' + book.createTime} className='w-[160px] h-[220px] bg-[#fefefed2] rounded-2xl shadow-2xl drop-shadow-xl cursor-pointer flex items-center justify-center relative group mr-5' onClick={()=>{
-                router.push(`/book/${book.id}`);
-              }}>
-                <img src={book.cover} alt={book.title} className='w-[160px] h-[220px] rounded-2xl object-cover' />
-                {
-                  book.cover.includes('cover.png') && <div className='absolute inset-0 flex flex-col items-center justify-center'>
-                    <div className='text-2xl font-bold text-white drop-shadow-2xl shadow-lg m-5 text-stroke-2'>{book.title}</div>
-                    <div className='text-xl font-bold text-white drop-shadow-2xl shadow-lg m-5 text-stroke-2'>{book.author ? book.author + ' 著': ''}</div>
-                  </div>
-                }
-              </div>
-            )
-          })
-        }
-        <Tooltip title="新建作品" color={'#2db7f5'}>
-        <div className='w-[160px] h-[220px] bg-[#fefefed2] rounded-2xl shadow-2xl cursor-pointer flex items-center justify-center group' onClick={()=>{
-          router.push('/newBook');
-        }}>
-          <img src='/images/add.png' alt='add' width={64} className='transform group-hover:rotate-90 transition duration-500' />
+      <div className="min-h-[100vh] p-10">
+        <div className="flex flex-row flex-wrap" >
+          {
+            books.sort((a,b)=>(a.createTime - b.createTime)).map((book, index)=>{
+              return (
+                <div key={book.title + '_' + book.createTime} className='w-[160px] h-[220px] bg-[#fefefed2] rounded-2xl shadow-2xl drop-shadow-xl cursor-pointer flex items-center justify-center relative group mr-5 mb-5' onClick={()=>{
+                  router.push(`/book/${book.id}`);
+                }}>
+                  <img src={book.cover} alt={book.title} className='w-[160px] h-[220px] rounded-2xl object-cover' />
+                  {
+                    book.cover.includes('cover.png') && <div className='absolute inset-0 flex flex-col items-center justify-center'>
+                      <div className='text-2xl font-bold text-white drop-shadow-2xl shadow-lg m-5 text-stroke-2'>{book.title}</div>
+                      <div className='text-xl font-bold text-white drop-shadow-2xl shadow-lg m-5 text-stroke-2'>{book.author ? book.author + ' 著': ''}</div>
+                    </div>
+                  }
+                </div>
+              )
+            })
+          }
+          <Tooltip title="新建作品" color={'#2db7f5'}>
+          <div className='w-[160px] h-[220px] bg-[#fefefed2] rounded-2xl shadow-2xl cursor-pointer flex items-center justify-center group' onClick={()=>{
+            router.push('/newBook');
+          }}>
+            <img src='/images/add.png' alt='add' width={64} className='transform group-hover:rotate-90 transition duration-500' />
+          </div>
+          </Tooltip>
         </div>
-        </Tooltip>
-        
       </div>
     </React.Fragment>
   )
