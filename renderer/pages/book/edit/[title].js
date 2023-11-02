35c2d8d0403ca0627549a6b84da9f63d769f6311
longwu <longwu@longwu.xyz>
2023-11-03 00:07:58 +0800

    update style ui

--- renderer/pages/book/edit/[title].js
+++ renderer/pages/book/edit/[title].js
@@ -23,14 +23,57 @@ export default function EditBook(props) {
   const [showTree, setShowTree] = useState(true);
   const [showText, setShowText] = useState(true);
   const [showCard, setShowCard] = useState(true);
+  const [color1, setColor1] = useState('#c0d4d7');
+  const [color2, setColor2] = useState('#e8e8e8');
+  const [color3, setColor3] = useState('#000');
+  const [bgImage, setBgImage] = useState(null);
+  const [bgOpacity, setBgOpacity] = useState(50);
 
+  const StylePanel = (
+    <div>
+      <div className="flex justify-start items-center mb-5">
+        <div className="mr-4">背景颜色：</div>
+        <ColorPicker showText value={color1} onChange={v=>setColor1(v.toHexString())} /> &nbsp;&nbsp;
+        <ColorPicker showText value={color2} onChange={v=>setColor2(v.toHexString())} />
+      </div>
+      <div className="flex justify-start items-center mb-5">
+        <div className="mr-4">背景透明：</div>
+        <input className="mr-5" type="range" min="0" max="100" value={bgOpacity} onChange={e=>setBgOpacity(e.target.value)} /> {bgOpacity + '%'}
+      </div>
+      <div className="flex justify-start items-center mb-5">
+        <div className="mr-4">背景图片：</div>
+        <Button size="small" className="mr-1">选取...</Button>
+        <Button size="small">清除</Button>
+      </div>
+      <div className="flex justify-start items-center mb-5">
+        <div className="mr-4">文字颜色：</div>
+        <ColorPicker showText value={color3} onChange={v=>setColor3(v.toHexString())} /> &nbsp;&nbsp;
+      </div>
+      <div className="flex justify-start items-center mb-5">
+        <div className="mr-4">字体选择：</div>
+      </div>
+      <div className="flex justify-center items-center mb-2">
+        <Button size="small" onClick={()=>{
+          setColor1('#c0d4d7');
+          setColor2('#e8e8e8');
+        }}>恢复默认</Button>
+      </div>
+    </div>
+  );
 
+  
   return (
     <React.Fragment>
       <Head>
         <title>{title} → 第1卷 风起云涌 | 第2章 苏醒 → 3,245字</title>
       </Head>
-      <div className="h-[100vh] text-center items-center">
+      <div 
+        style={{ 
+          backgroundImage: `linear-gradient(to top right, ${color1}, ${color2})`,
+          color: color3,
+        }}
+        className={`h-[100vh] text-center items-center overflow-hidden`}
+      >
         <div className="w-[600px] rounded-full bg-white m-auto rounded-t-none h-[26px] flex items-center text-center justify-center relative">
           <Tooltip title="返回书库" color={"blue"}>
             <Button
@@ -91,7 +134,7 @@ export default function EditBook(props) {
             </Button>
           </Tooltip> */}
           <Tooltip title="界面样式" placement="right" color={"blue"}>
-            <Popover placement="bottomLeft" title={'界面样式调整'} content={stylePanel} trigger="click">
+            <Popover placement="bottomLeft" title={'界面样式调整'} content={StylePanel} trigger="click">
               <Button
                 className={`cursor-pointer mr-2 p-1 hover:bg-blue-200 rounded border-none`}
               >
@@ -115,7 +158,11 @@ export default function EditBook(props) {
             className={styles.leftPane}
             minSize={180}
           >
-            <div className="bg-white h-full mt-2 w-full rounded-r-lg overflow-hidden border-2">
+            <div
+              style={{
+                backgroundColor: `rgba(255,255,255,${bgOpacity/100})`,
+              }}
+            className={`h-full mt-2 w-full rounded-r-lg overflow-hidden border-2`}>
               <div className="bg-gray-500 bg-opacity-10 rounded-lg m-1">
                 目录
               </div>
@@ -128,14 +175,21 @@ export default function EditBook(props) {
           >
             <Allotment vertical separator={false}>
               <Allotment.Pane>
-                <div className="bg-white h-full mt-2 w-full rounded-lg overflow-hidden border-2">
+                <div
+                  style={{
+                    backgroundColor: `rgba(255,255,255,${bgOpacity/100})`,
+                  }}
+                  className={`h-full mt-2 w-full rounded-lg overflow-hidden border-2`}>
                   <div className="bg-gray-500 bg-opacity-10 rounded-lg m-1">
                     大纲
                   </div>
                 </div>
               </Allotment.Pane>
               <Allotment.Pane>
-                <div className="bg-white h-full mt-[2px] w-full rounded-lg overflow-hidden border-2">
+                <div
+                  style={{
+                    backgroundColor: `rgba(255,255,255,${bgOpacity/100})`,
+                  }} className={` h-full mt-[2px] w-full rounded-lg overflow-hidden border-2`}>
                   <div className="bg-gray-500 bg-opacity-10 rounded-lg m-1">
                     细纲 / 章纲
                   </div>
@@ -149,7 +203,10 @@ export default function EditBook(props) {
             className={styles.rightPane}
             minSize={200}
           >
-            <div className="bg-white h-full mt-2 w-full rounded-lg overflow-hidden border-2">
+            <div
+              style={{
+                backgroundColor: `rgba(255,255,255,${bgOpacity/100})`,
+              }} className={` h-full mt-2 w-full rounded-lg overflow-hidden border-2`}>
               <div className="bg-gray-500 bg-opacity-10 rounded-lg m-1">
                 正文
               </div>
@@ -161,7 +218,10 @@ export default function EditBook(props) {
             className={styles.rightPane}
             minSize={180}
           >
-            <div className="bg-white h-full mt-2 w-full rounded-l-lg overflow-hidden border-2">
+            <div
+              style={{
+                backgroundColor: `rgba(255,255,255,${bgOpacity/100})`,
+              }} className={` h-full mt-2 w-full rounded-l-lg overflow-hidden border-2`}>
               <div className="bg-gray-500 bg-opacity-10 rounded-lg m-1">
                 卡片
               </div>
@@ -173,17 +233,3 @@ export default function EditBook(props) {
   );
 }
 
-const stylePanel = (
-  <div>
-    <div className="flex justify-start items-center mb-3">
-      <div className="mr-4">背景颜色：</div>
-      <ColorPicker /> &nbsp;&nbsp;
-      <ColorPicker />
-      </div>
-    <div className="flex justify-start items-center mb-3">字体选择：</div>
-    <div>
-      <Button className="bg-blue-500 m-3 mb-1 " type="primary">应用</Button>
-      <Button>还原</Button>
-    </div>
-  </div>
-);
