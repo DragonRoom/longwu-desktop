8977f229c5c8b579578c67a58ca801ea69f74487
longwu <longwu@longwu.xyz>
2024-02-23 22:11:44 +0800

    Refactor ContentTree component and useBase hook

--- renderer/components/BookEditor/ContentTree.js
+++ renderer/components/BookEditor/ContentTree.js
@@ -8,6 +8,7 @@ import { formatNumber } from "./utils";
 import { useBase } from "../../hooks/useBase";
 import { useCurrent } from "../../hooks/useCurrent";
 import { useWordCnt } from "../../hooks/useWordCnt";
+import { promisify } from "../../hooks/utils";
 
 
 export default function ContentTree() {
@@ -15,9 +16,54 @@ export default function ContentTree() {
   const [newName, setNewName] = useState('');
   const [volume, setVolume] = useState('');
   const [chapter, setChapter] = useState('');
-  const { contentTree, title, setShowText } = useBase();
+  const { contentTree, title, setShowText, treeUpdater, setContentTree, iconStyle } = useBase();
   const { setCurrentVolume, setCurrentChapter, selectedKeys } = useCurrent();
   const { chapters } = useWordCnt();
+
+  useEffect(() => {
+    if (!title) return;
+
+    // load contentTree from file
+    const func = async () => {
+      window.ipc.send('get-volume-list', title);
+      let arg = await promisify(window.ipc.on, 'get-volume-list');
+      console.log('get-volume-list', arg);
+      let _contentTree = [];
+      let volumeTitles = Object.values(arg.data).map(v=>v.title);
+      console.log('volumeTitles', volumeTitles);
+      for (let i=0; i<volumeTitles.length; i++) {
+        _contentTree.push({
+          title: volumeTitles[i],
+          volume: (i + 1).toString(),
+          key: i.toString(),
+          icon: <img src="/images/volume.svg" width={22} alt="卷" />,
+          words: 0,
+          children: [],
+        });
+
+        window.ipc.send('list-chapters', {bookTitle: title, volumeNumber: (i + 1).toString()});
+        let arg2 = await promisify(window.ipc.on, 'list-chapters');
+        console.log('list-chapters', arg2);
+        let chapterTitles = Object.values(arg2.data).map(v=>v.title);
+        console.log('chapterTitles', chapterTitles);
+        for (let j=0; j<chapterTitles.length; j++) {
+          _contentTree[i].children.push({
+            title: chapterTitles[j],
+            volume: (i + 1).toString(),
+            chapter: (j + 1).toString(),
+            key: i.toString() + '-' + j.toString(),
+            icon: iconStyle,
+            words: 0,
+          });
+        }
+      }
+
+      console.log('contentTree', _contentTree);
+      setContentTree(() => _contentTree);
+    }
+
+    func().then(console.log('update contentTree finish')).catch(console.error);
+  }, [treeUpdater, title]);
 
 
   const RenamePanel = (
--- renderer/hooks/useBase.js
+++ renderer/hooks/useBase.js
@@ -15,50 +15,7 @@ function _useBase() {
   const [treeUpdater, setTreeUpdater] = useState(0);
   const [contentTree, setContentTree] = useState([]);
 
-  useEffect(() => {
-    if (!title) return;
 
-    // load contentTree from file
-    const func = async () => {
-      window.ipc.send('get-volume-list', title);
-      let arg = await promisify(window.ipc.on, 'get-volume-list');
-      console.log('get-volume-list', arg);
-      let _contentTree = [];
-      let volumeTitles = Object.values(arg.data).map(v=>v.title);
-      console.log('volumeTitles', volumeTitles);
-      for (let i=0; i<volumeTitles.length; i++) {
-        _contentTree.push({
-          title: volumeTitles[i],
-          volume: (i + 1).toString(),
-          key: i.toString(),
-          icon: <img src="/images/volume.svg" width={22} alt="卷" />,
-          words: 0,
-          children: [],
-        });
-
-        window.ipc.send('list-chapters', {bookTitle: title, volumeNumber: (i + 1).toString()});
-        let arg2 = await promisify(window.ipc.on, 'list-chapters');
-        console.log('list-chapters', arg2);
-        let chapterTitles = Object.values(arg2.data).map(v=>v.title);
-        console.log('chapterTitles', chapterTitles);
-        for (let j=0; j<chapterTitles.length; j++) {
-          _contentTree[i].children.push({
-            title: chapterTitles[j],
-            volume: (i + 1).toString(),
-            chapter: (j + 1).toString(),
-            key: i.toString() + '-' + j.toString(),
-            icon: iconStyle,
-            words: 0,
-          });
-        }
-      }
-
-      console.log('contentTree', _contentTree);
-      setContentTree(_contentTree);
-    }
-
-    func().then(console.log('update contentTree finish')).catch(console.error);
-  }, [treeUpdater, title]);
 
   return {
     title,
@@ -81,6 +38,8 @@ function _useBase() {
     setShowNewVolumePanel,
     showNewChapterPanel,
     setShowNewChapterPanel,
+
+    iconStyle,
   }
 }
 
