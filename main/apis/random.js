2ca9f9e5b4ae148e49641d6df9f2710176bea698
longwu <longwu@longwu.xyz>
2024-03-04 23:24:42 +0800

    update new name system

--- main/apis/random.js
+++ main/apis/random.js
@@ -1,4 +1,5 @@
 const randomName = require('chinese-random-name');
+const randomSkill = require("chinese-random-skill");
 import { ipcMain } from 'electron'
 
 export function initRandomApi() {
@@ -15,6 +16,21 @@ export function initRandomApi() {
       event.reply('random-name', {success: true, data: rets});
     } catch (e) {
       event.reply('random-name', {success: false, message: e.message});
+    }
+  });
+
+  ipcMain.on('random-skill', async (event, arg) => {
+    console.log('random-skill', arg.count);
+    try {
+      let rets = [];
+      for (let i=0; i<arg.count; i++) {
+        let name = randomSkill.generate();
+        console.log(name);
+        rets.push(name);
+      }
+      event.reply('random-skill', {success: true, data: rets});
+    } catch (e) {
+      event.reply('random-skill', {success: false, message: e.message});
     }
   });
 }
--- renderer/components/BookEditor/RandomPanel.js
+++ renderer/components/BookEditor/RandomPanel.js
@@ -1,37 +1,66 @@
 // Random Name Panel for Book Editor
 
-import { Card } from "antd";
+import { Button, Card, Tabs, Tag, message } from "antd";
 import { useEffect, useState } from "react";
+import { getName, getDao, getBook, getSkill,
+  getCreature, getMaterial, getTalisman, 
+  getAlchemy, getClan, getNation, 
+  getLocation, getZone } from "random_chinese_fantasy_names";
+
+import copy from 'copy-to-clipboard';
+
+const types = ['人名','法号','功法','秘籍','生灵','材料','法宝','丹药','门派','国家','地点','大陆'];
+const rarityType = ['common','uncommon','rare','epic','legendary', 'mythic', 'exotic'];
+const colors = ['','green','blue','purple','orange','red','gold'];
+
+const NamePanel = ({_type}) => {
+  const [names, setNames] = useState([]);
+  const [updater, setUpdater] = useState(0);
+  useEffect(() => {
+    const func = {
+      '人名': getName,
+      '法号': getDao,
+      '功法': getSkill,
+      '秘籍': getBook,
+      '生灵': getCreature,
+      '材料': getMaterial,
+      '法宝': getTalisman,
+      '丹药': getAlchemy,
+      '门派': getClan,
+      '国家': getNation,
+      '地点': getLocation,
+      '大陆': getZone,
+    };
+    console.log('type', _type);
+    let ret = func[_type](60);
+    setNames(ret);
+  }, [_type, updater]);
+  return <Card className="w-[500px]" >
+    <Button className="mb-2" onClick={()=>setUpdater(Date.now())}>随机生成</Button>
+    <div className="flex flex-wrap gap-1">
+      {names.map((name, index) => {
+        return <Tag onClick={()=>{
+          copy(name.name ? name.name : name);
+          message.success('已复制到剪贴板');
+        }} color={name.rarity ? colors[rarityType.indexOf(name.rarity)] : '' } className="cursor-pointer" key={index}>{name.name ? name.name : name}</Tag>
+      })}
+    </div>
+  </Card>
+}
 
 export const RandomPanel = () => {
-  const [names, setNames] = useState([]);
-  const [nameLength, setNameLength] = useState(3);
-  const [nameCount, setNameCount] = useState(10);
-
-  useEffect(()=>{
-    console.log('random-name', nameLength, nameCount);
-    window.ipc.send('random-name', {length: nameLength, count: nameCount});
-    window.ipc.on('random-name', (arg) => {
-      console.log('random-name', arg);
-      if (arg.success) {
-        setNames(arg.data);
-      }
-    });
-  }, [nameLength, nameCount]);
-
   return <div>
-    <Card title="随机中文名" className="w-[300px]">
-      姓名长度：<input type="number" value={nameLength} onChange={e=>setNameLength(e.target.value)} className="text-center border rounded-lg"/>
-      <br />
-      起名数量：<input type="number" value={nameCount} onChange={e=>setNameCount(e.target.value)} className="text-center border rounded-lg"/>
-      <br />
-      <div className="mt-2 flex gap-1 flex-wrap max-w-[260px]">
-      {
-        names.map((name, index) => {
-          return <div key={index} className="bg-gray-200 rounded-lg p-1">{name}</div>
-        })
-      }
-      </div>
-    </Card>
+    <Tabs defaultActiveKey="0"
+      className="w-[500px]"
+      size="small"
+      type="card"
+      items={types.map((name, index) => {
+        return {
+          key: index.toString(),
+          label: name,
+          children: <NamePanel _type={name} />
+        }
+      })}
+    />
   </div>
 }
